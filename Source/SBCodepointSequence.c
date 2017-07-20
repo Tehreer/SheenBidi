c75a94cb9eff52be3a4725a40360054223cf30dc
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2017-07-20 12:30:21 +0500

    [lib] Implemented UTF-8 decoder based on lookup table...

--- Source/SBCodepointSequence.c
+++ Source/SBCodepointSequence.c
@@ -23,6 +23,56 @@
 #include "SBBase.h"
 #include "SBCodepointSequence.h"
 
+typedef struct {
+    SBUInt8 valid;
+    SBUInt8 total;
+    SBUInt8 start;
+    SBUInt8 end;
+} _SBUTF8State;
+
+static const _SBUTF8State _SBUTF8StateTable[9] = {
+    {1,0,0x00,0x00}, {0,0,0x00,0x00}, {1,2,0x80,0xBF}, {1,3,0xA0,0xBF}, {1,3,0x80,0xBF},
+    {1,3,0x80,0x9F}, {1,4,0x90,0xBF}, {1,4,0x80,0xBF}, {1,4,0x80,0x8F}
+};
+
+static const SBUInt8 _SBUTF8LookupTable[256] = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+/* LEAD: -- 80..BF -- */
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1,
+/* LEAD: -- C0..C1 -- */
+    1, 1,
+/* LEAD: -- C2..DF -- */
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+/* LEAD: -- E0..E0 -- */
+    3,
+/* LEAD: -- E1..EC -- */
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+/* LEAD: -- ED..ED -- */
+    5,
+/* LEAD: -- EE..EF -- */
+    4, 4,
+/* LEAD: -- F0..F0 -- */
+    6,
+/* LEAD: -- F1..F3 -- */
+    7, 7, 7,
+/* LEAD: -- F4..F4 -- */
+    8,
+/* LEAD: -- F5..F7 -- */
+    1, 1, 1,
+/* LEAD: -- F8..FB -- */
+    1, 1, 1, 1,
+/* LEAD: -- FC..FD -- */
+    1, 1,
+/* LEAD: -- FE..FF -- */
+    1, 1
+};
+
 static SBCodepoint _SBGetUTF8CodepointAt(const SBCodepointSequence *codepointSequence, SBUInteger *stringIndex);
 static SBCodepoint _SBGetUTF8CodepointBefore(const SBCodepointSequence *codepointSequence, SBUInteger *stringIndex);
 static SBCodepoint _SBGetUTF16CodepointAt(const SBCodepointSequence *codepointSequence, SBUInteger *stringIndex);
@@ -99,110 +149,41 @@ static SBCodepoint _SBGetUTF8CodepointAt(const SBCodepointSequence *sequence, SB
 {
     const SBUInt8 *buffer = sequence->stringBuffer;
     SBUInteger length = sequence->stringLength;
-    SBCodepoint result;
     SBUInt8 lead;
+    _SBUTF8State state;
+    SBUInteger limit;
+    SBCodepoint codepoint;
 
-    /**
-     * Reference: https://en.wikipedia.org/wiki/UTF-8
-     *
-     * +======+=========+==========+=======+==========+==========+==========+==========+
-     * | Bits | First   | Last     | Bytes | Byte 1   | Byte 2   | Byte 3   | Byte 4   |
-     * +======+=========+==========+=======+==========+==========+==========+==========+
-     * |  7   | U+0000  | U+007F   |   1   | 0xxxxxxx |          |          |          |
-     * +------+---------+----------+-------+----------+----------+----------+----------+
-     * |  11  | U+0080  | U+07FF   |   2   | 110xxxxx | 10xxxxxx |          |          |
-     * +------+---------+----------+-------+----------+----------+----------+----------+
-     * |  16  | U+0800  | U+FFFF   |   3   | 1110xxxx | 10xxxxxx | 10xxxxxx |          |
-     * +------+---------+----------+-------+----------+----------+----------+----------+
-     * |  21  | U+10000 | U+1FFFFF |   4   | 11110xxx | 10xxxxxx | 10xxxxxx | 10xxxxxx |
-     * +------+---------+----------+-------+----------+----------+----------+----------+
-     */
+    lead = buffer[*index];
+    state = _SBUTF8StateTable[_SBUTF8LookupTable[lead]];
+    limit = *index + state.total;
 
-    lead = buffer[(*index)++];
-    result = SBCodepointFaulty;
-
-    if (lead < 0x80) {
-        result = lead;
-    } else if (lead < 0xC0) {
-        /* Lone continuation byte. */
-    } else if (lead < 0xE0) {
-        SBCodepoint codepoint;
-        SBCodepoint bytes[2];
-
-        bytes[0] = lead & 0x1F;
-
-        if (((*index) < length && (bytes[1] = buffer[*index] - 0x80) < 0x40)) {
-           ++(*index);
-
-            codepoint = (bytes[0] << 6)
-                      | (bytes[1] << 0);
-
-            if (codepoint >= 0x0080) {
-                result = codepoint;
-            }
-        }
-    } else if (lead < 0xF0) {
-        SBCodepoint codepoint;
-        SBCodepoint bytes[3];
-
-        bytes[0] = lead & 0xF;
-
-        if (((*index) < length && (bytes[1] = buffer[*index] - 0x80) < 0x40) &&
-          (++(*index) < length && (bytes[2] = buffer[*index] - 0x80) < 0x40)) {
-           ++(*index);
-
-            codepoint = (bytes[0] << 12)
-                      | (bytes[1] <<  6)
-                      | (bytes[2] <<  0);
-
-            if (codepoint >= 0x0800 && !SBCodepointInRange(codepoint, 0xD800, 0xDFFF)) {
-                result = codepoint;
-            }
-        }
-    } else if (lead < 0xF8) {
-        SBCodepoint codepoint;
-        SBCodepoint bytes[4];
-
-        bytes[0] = lead & 0x7;
-
-        if (((*index) < length && (bytes[1] = buffer[*index] - 0x80) < 0x40) &&
-          (++(*index) < length && (bytes[2] = buffer[*index] - 0x80) < 0x40) &&
-          (++(*index) < length && (bytes[3] = buffer[*index] - 0x80) < 0x40)) {
-           ++(*index);
-
-            codepoint = (bytes[0] << 18)
-                      | (bytes[1] << 12)
-                      | (bytes[2] <<  6)
-                      | (bytes[3] <<  0);
-
-            if (SBCodepointInRange(codepoint, 0x10000, 0x10FFFF)) {
-                result = codepoint;
-            }
-        }
-    } else if (lead < 0xFC) {
-        /* Invalid byte sequence. */
-
-        if (((*index) < length && SBUInt8InRange(buffer[*index], 0x80, 0xBF)) &&
-          (++(*index) < length && SBUInt8InRange(buffer[*index], 0x80, 0xBF)) &&
-          (++(*index) < length && SBUInt8InRange(buffer[*index], 0x80, 0xBF)) &&
-          (++(*index) < length && SBUInt8InRange(buffer[*index], 0x80, 0xBF))) {
-          ++(*index);
-            /* The above statements will skip valid continuation bytes. */
-        }
-    } else if (lead < 0xFE) {
-        /* Invalid byte sequence. */
-
-        if (((*index) < length && SBUInt8InRange(buffer[*index], 0x80, 0xBF)) &&
-          (++(*index) < length && SBUInt8InRange(buffer[*index], 0x80, 0xBF)) &&
-          (++(*index) < length && SBUInt8InRange(buffer[*index], 0x80, 0xBF)) &&
-          (++(*index) < length && SBUInt8InRange(buffer[*index], 0x80, 0xBF)) &&
-          (++(*index) < length && SBUInt8InRange(buffer[*index], 0x80, 0xBF))) {
-           ++(*index);
-            /* The above statements will skip valid continuation bytes. */
-        }
+    if (limit > length) {
+        limit = length;
+        state.valid = SBFalse;
     }
 
-    return result;
+    codepoint = lead & (0x7F >> state.total);
+
+    while (++(*index) < limit) {
+        SBUInt8 byte = buffer[*index];
+
+        if (byte >= state.start && byte <= state.end) {
+            codepoint = (codepoint << 6) | (byte & 0x3F);
+        } else {
+            state.valid = SBFalse;
+            break;
+        }
+
+        state.start = 0x80;
+        state.end = 0xBF;
+    }
+
+    if (state.valid) {
+        return codepoint;
+    }
+
+    return SBCodepointFaulty;
 }
 
 static SBCodepoint _SBGetUTF8CodepointBefore(const SBCodepointSequence *codepointSequence, SBUInteger *stringIndex)
