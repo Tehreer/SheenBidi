e1568d9395370082edab36b67b81371b814e6517
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2018-04-15 17:14:33 +0500

    [test] Added script tester...

---
+++ Tools/Tester/ScriptTester.cpp
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2018 Muhammad Tayyab Akram
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+extern "C" {
+#include <SBBase.h>
+#include <SBConfig.h>
+#include <Source/SBScriptLookup.h>
+}
+
+#include <cstdint>
+#include <iostream>
+#include <string>
+
+#include <Parser/PropertyValueAliases.h>
+#include <Parser/Scripts.h>
+
+#include "Utilities/Convert.h"
+#include "Utilities/Unicode.h"
+
+#include "Configuration.h"
+#include "ScriptTester.h"
+
+using namespace std;
+using namespace SheenBidi::Parser;
+using namespace SheenBidi::Tester;
+using namespace SheenBidi::Tester::Utilities;
+
+static const string &BIDI_CLASS_DEFAULT = "L";
+
+ScriptTester::ScriptTester(const Parser::Scripts &scripts, const Parser::PropertyValueAliases &propertyValueAliases)
+    : m_scripts(scripts)
+    , m_propertyValueAliases(propertyValueAliases)
+{
+}
+
+void ScriptTester::test() {
+#ifdef SB_CONFIG_UNITY
+    cout << "Cannot run script tester in unity mode." << endl;
+#else
+    cout << "Running script tester." << endl;
+
+    size_t failCounter = 0;
+
+    for (uint32_t codePoint = 0; codePoint <= Unicode::MAX_CODE_POINT; codePoint++) {
+        const string &scriptName = m_scripts.scriptForCodePoint(codePoint);
+        const string &expScript = m_propertyValueAliases.abbreviationForScript(scriptName);
+
+        SBScript valScript = SBScriptDetermine(codePoint);
+        const string &genScript = Convert::scriptToString(valScript);
+
+        if (expScript != genScript) {
+            if (Configuration::DISPLAY_ERROR_DETAILS) {
+                cout << "Invalid script found: " << endl
+                     << "  Code Point: " << codePoint << endl
+                     << "  Expected Script: " << expScript << endl
+                     << "  Generated Script: " << genScript << endl;
+            }
+                
+            failCounter++;
+        }
+    }
+
+    cout << failCounter << " error/s." << endl;
+#endif
+
+    cout << endl;
+}
---
+++ Tools/Tester/ScriptTester.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2018 Muhammad Tayyab Akram
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef SHEENBIDI_TESTER_SCRIPT_TESTER_H
+#define SHEENBIDI_TESTER_SCRIPT_TESTER_H
+
+#include <string>
+#include <vector>
+
+#include <Parser/PropertyValueAliases.h>
+#include <Parser/Scripts.h>
+
+namespace SheenBidi {
+namespace Tester {
+
+class ScriptTester {
+public:
+    ScriptTester(const Parser::Scripts &scripts, const Parser::PropertyValueAliases &propertyValueAliases);
+
+    void test();
+
+private:
+    const Parser::Scripts &m_scripts;
+    const Parser::PropertyValueAliases &m_propertyValueAliases;
+};
+
+}
+}
+
+#endif
