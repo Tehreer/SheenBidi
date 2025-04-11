13cdefab586be1650de05c2955efe3b09d13049a
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2025-04-11 20:34:30 +0500

    Update copyright years in source code files

--- Tools/Tester/Utilities/Convert.cpp
+++ Tools/Tester/Utilities/Convert.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2020 Muhammad Tayyab Akram
+ * Copyright (C) 2015-2024 Muhammad Tayyab Akram
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
